821d544090adb02c3489cf451bf9aa418da2df95
chizbet2 <ikechuzoho@gmail.com>
2023-05-13 21:17:41 -0600

    fff

--- docker/compose-networks/site/index.php
+++ docker/compose-networks/site/index.php
@@ -8,16 +8,16 @@
         foreach ($price_items as $price_item) {
           echo "<li>$price_item->price</li>";
         }
-    //   ?>
-    // </ul>
-    // <h2>Core apparel:</h2>
-    // <ul>
-    //   <?php
-    //     $json = file_get_contents('http://apparel');
-    //     $apparel_items = json_decode($json);
-    //     foreach ($apparel_items as $apparel_item) {
-    //       echo "<li>$apparel_item->name</li>";
-    //     }
+      ?>
+    </ul>
+    <h2>Core apparel:</h2>
+    <ul>
+      <?php
+        $json = file_get_contents('http://apparel');
+        $apparel_items = json_decode($json);
+        foreach ($apparel_items as $apparel_item) {
+          echo "<li>$apparel_item->name</li>";
+        }
       ?>
     </ul>
   </body>
